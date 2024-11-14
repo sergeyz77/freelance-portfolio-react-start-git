34ea02bd8678fe7deddfa0de4f63c55088908d89
sergeyz77 <sergey-z-2012@mail.ru>
2024-11-14 09:39:05 +0300

    com-navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/main.css";
-import sun from "./img/icons/sun.svg"
-import moon from "./img/icons/moon.svg"
+
 
 import instagram from "./img/icons/instagram.svg"
 import linkedIn from "./img/icons/linkedIn.svg"
 import twitter from "./img/icons/twitter.svg"
 import gitHub from "./img/icons/gitHub.svg"
 import vk from "./img/icons/vk.svg"
+
+
+import Navbar from "./components/navbar/Navbar";
 
 
 
@@ -16,83 +18,18 @@ function App() {
 
     <div className="App">
 
-      <nav className="nav">
 
-        <div className="container">
 
-          <div className="nav-row">
-            <a href="./index.html" className="logo">
-              <strong>Freelancer</strong> portfolio
-            </a>
+      <Navbar />
 
-            <button className="dark-mode-btn">
-              <img
-                src={sun}
-                alt="Light mode"
-                className="dark-mode-btn__icon"
-              />
-              <img
-                src={moon}
-                alt="Dark mode"
-                className="dark-mode-btn__icon"
-              />
-            </button>
 
-            <ul className="nav-list">
-
-              <li className="nav-list__item">
-                <a
-                  href="./index.html"
-                  className="nav-list__link nav-list__link--active"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./projects.html" className="nav-list__link">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./contacts.html" className="nav-list__link">
-                  Contacts
-                </a>
-              </li>
-
-            </ul>
-
-          </div>
-
-        </div>
-
-      </nav>
+  
 
 
 
 
 
-
-
-
-
-      <header className="header">
-
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            <strong>
-              Hi, my name is <em>Yuri</em>
-            </strong>
-            <br />a frontend developer
-          </h1>
-          <div className="header__text">
-            <p>with passion for learning and creating.</p>
-          </div>
-          <a href="#!" className="btn">
-            Download CV
-          </a>
-        </div>
-
-      </header>
+   
 
 
       <main className="section">
