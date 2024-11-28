ee8bf34dac8b026e0ec29468eeb7cc9ab84de313
sergeyz77 <sergey-z-2012@mail.ru>
2024-11-28 11:18:10 +0300

    com-подключение роутера, маршруты для страниц

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./styles/main.css";
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 
 import Footer from "./components/footer/Footer";
 
 import Project from "./pages/Project";
-// import Contacts from "./pages/Contacts";
-// import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 
 
@@ -18,24 +18,29 @@ function App() {
 
      <div className="App">
 
-
+<Router>
 
       <Navbar />
 
-   
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contacts" element={<Contacts />} />
+      </Routes>
       {/* <Home /> */}
 
-      <Project/>
+      {/* <Project/> */}
 
 
       {/* <Projects /> */}
 
       {/* <Contacts /> */}
 
-
+      
       <Footer />
 
-
+      </Router>
 
 
 
